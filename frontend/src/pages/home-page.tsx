240ee039807aae20de422ca1278cea9c36b3e8cb
NurAhissami <nurahi10@gmail.com>
2024-06-30 02:43:06 +0200

    feat(home-page): create a simple home page

---
+++ frontend/src/pages/home-page.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+export const HomePage: FC = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to Pearson Course Management</h1>
+      <p>Use the navigation links above to add a course or add a student.</p>
+    </div>
+  );
+};
