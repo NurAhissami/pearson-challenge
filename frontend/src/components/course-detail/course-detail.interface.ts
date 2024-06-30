70b272c35818b7188348c46328bac9770e5e5e08
NurAhissami <nurahi10@gmail.com>
2024-06-30 02:57:14 +0200

    feat(course-detail): update component

--- frontend/src/components/course-detail/course-detail.interface.ts
+++ frontend/src/components/course-detail/course-detail.interface.ts
@@ -1,11 +1,9 @@
-import { Dispatch, SetStateAction } from "react";
-import { Course } from "../types";
+import { Course, Student } from "../types";
 
 export interface CourseDetailProps {
   course: Course | null;
   onEdit: (course: Course) => void;
   onDelete: (id: number) => void;
-  onAddStudent: (courseId: number, studentName: string) => void;
-  studentName: string;
-  setStudentName: Dispatch<SetStateAction<string>>;
+  handleRemoveStudentFromCourse: (courseId: number, studentId: number) => void;
+  updateCourseStudents: (courseId: number, students: Student[]) => void;
 }
--- frontend/src/components/course-detail/course-detail.tsx
+++ frontend/src/components/course-detail/course-detail.tsx
@@ -1,43 +1,74 @@
-import { FC, FormEvent, useState } from "react";
+import { FC, useState, useEffect, useRef } from "react";
+import { Student } from "../types";
+import { StudentList } from "../student-list";
+import { addStudentToCourse, fetchStudents } from "../../utils";
 import { CourseDetailProps } from "./course-detail.interface";
-import { Student } from "../student/student";
 
 export const CourseDetail: FC<CourseDetailProps> = ({
   course,
   onEdit,
   onDelete,
-  onAddStudent,
-  studentName,
-  setStudentName,
+  handleRemoveStudentFromCourse,
+  updateCourseStudents,
 }) => {
-  const [expandedCourses, setExpandedCourses] = useState<{
-    [key: number]: boolean;
-  }>({});
+  const [students, setStudents] = useState<Student[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [selectedStudent, setSelectedStudent] = useState<number | null>(null);
+  const [filteredStudents, setFilteredStudents] = useState<Student[]>([]);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const toggleExpand = (courseId: number) => {
-    setExpandedCourses((prevState) => ({
-      ...prevState,
-      [courseId]: !prevState[courseId],
-    }));
+  useEffect(() => {
+    fetchStudents()
+      .then((response) => {
+        const sortedStudents = response.data.sort((a: Student, b: Student) =>
+          a.name.localeCompare(b.name)
+        );
+        setStudents(sortedStudents);
+        setFilteredStudents(sortedStudents.slice(0, 5));
+      })
+      .catch((error) => console.error("Error fetching students", error));
+  }, []);
+
+  useEffect(() => {
+    const availableStudents = students.filter(
+      (student) => !course?.students?.some((s) => s.id === student.id)
+    );
+    if (searchTerm === "") {
+      setFilteredStudents(availableStudents.slice(0, 5));
+    } else {
+      setFilteredStudents(
+        availableStudents
+          .filter((student) =>
+            student.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .slice(0, 5)
+      );
+    }
+  }, [searchTerm, students, course]);
+
+  const handleAddStudentToCourse = (studentId: number) => {
+    if (course) {
+      addStudentToCourse(course.id, studentId)
+        .then((response) => {
+          const updatedStudents = [...course.students, response.data].sort(
+            (a, b) => a.name.localeCompare(b.name)
+          );
+          updateCourseStudents(course.id, updatedStudents);
+          setSelectedStudent(null);
+          setSearchTerm("");
+          if (inputRef.current) {
+            inputRef.current.value = "";
+            inputRef.current.focus();
+          }
+        })
+        .catch((error) =>
+          console.error("Error adding student to course", error)
+        );
+    }
   };
 
-  if (!course) {
-    return <div className="course-detail">Select a course to see details</div>;
-  }
-
-  const handleEdit = () => {
-    onEdit(course);
-  };
-
-  const handleDelete = () => {
-    onDelete(course.id);
-  };
-
-  const handleAddStudent = (e: FormEvent) => {
-    e.preventDefault();
-    onAddStudent(course.id, studentName);
-    setStudentName("");
-  };
+  if (!course) return <div className="course-detail">Select a course</div>;
 
   return (
     <div className="course-detail">
@@ -47,29 +78,61 @@ export const CourseDetail: FC<CourseDetailProps> = ({
           <button
             data-testid="course-detail-edit-button"
             className="course-detail__button-edit"
-            onClick={handleEdit}
+            onClick={() => onEdit(course)}
           ></button>
           <button
             data-testid="course-detail-delete-button"
             className="course-detail__button-delete"
-            onClick={handleDelete}
+            onClick={() => onDelete(course.id)}
           ></button>
         </div>
       </div>
-
       <p className="course-detail__text">{course.description}</p>
       <p className="course-detail__text-date">
         <strong>{course.schedule}</strong>
       </p>
-
-      <Student
-        course={course}
-        toggleExpand={toggleExpand}
-        expandedCourses={expandedCourses[course.id]}
-        studentName={studentName}
-        setStudentName={setStudentName}
-        handleAddStudent={handleAddStudent}
-      />
+      <div className="course-detail__add-student">
+        <h4 className="course-detail__add-student-title">
+          Add Student to Course
+        </h4>
+        <div className="course-detail__add-student-container">
+          <input
+            type="text"
+            ref={inputRef}
+            className="course-detail__add-student-container--input"
+            placeholder="Search for a student"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            onKeyPress={(e) => {
+              if (e.key === "Enter" && filteredStudents.length > 0) {
+                handleAddStudentToCourse(filteredStudents[0].id);
+              }
+            }}
+          />
+          {isFocused && filteredStudents.length > 0 && (
+            <ul className="course-detail__add-student-container--list">
+              {filteredStudents.map((student) => (
+                <li
+                  key={student.id}
+                  onMouseDown={() => handleAddStudentToCourse(student.id)}
+                >
+                  {student.name}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        {course?.students?.length !== 0 && (
+          <StudentList
+            students={course.students}
+            onDelete={(studentId) =>
+              handleRemoveStudentFromCourse(course.id, studentId)
+            }
+          />
+        )}
+      </div>
     </div>
   );
 };
