f8f444e543a6a14b2ad8ab7cef07eb5711dc191b
NurAhissami <nurahi10@gmail.com>
2024-06-30 02:57:33 +0200

    feat(index): create index export

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,7 @@
+export * from "./course-detail";
+export * from "./course-form";
+export * from "./course-list";
+export * from "./input";
+export * from "./nav-link";
+export * from "./student-form";
+export * from "./student-list";
---
+++ frontend/src/utils/index.ts
@@ -0,0 +1,3 @@
+export * from "./api";
+export * from "./date-formatter";
+export * from "./validate-date";
